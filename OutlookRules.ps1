e23adacd93f87802782b3bd1de10443fc9b9b2a1
Manamius <122295366+Manamius@users.noreply.github.com>
2026-04-13 15:08:18 -0500

    Update OutlookRules.ps1
    Add comments

--- OutlookRules.ps1
+++ OutlookRules.ps1
@@ -1,4 +1,7 @@
-
+<#
+Author: Manamius
+Description: Grab a list of Inbox Rules associated with users in a 365 tenant. Will need to connect to Exchange Online to run
+#>
 
 $mailboxes = (get-mailbox -resultsize unlimited).UserPrincipalName
 
