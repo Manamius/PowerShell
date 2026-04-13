91da4d9e13985294888ed7a0f71a08f7bc93e1ac
Manamius <122295366+Manamius@users.noreply.github.com>
2026-04-13 15:06:16 -0500

    Update ADGroups.ps1
    Add comments

--- ADGroups.ps1
+++ ADGroups.ps1
@@ -1,8 +1,13 @@
+<#
+Author: Manamius
+Description: Find all the distribution groups in AD and export a list of members to a CSV file in the temp directory
+#>
+
 $DistroGroups = Get-ADGroup -Filter 'GroupCategory -eq "Distribution"' | select-object name
 
 foreach ($group in $DistroGroups)
 {
-    get-adgroupmember -identity "Design Group" | select-object name, $group
+    get-adgroupmember | select-object name, $group
 }
 
 Export-CSV -path c:\temp\DistroGroupTest.csv
